3e203020062a69f0fcbd318170d67d56e1cba64e
Pavan-Parker <31162938+Pavan-Parker@users.noreply.github.com>
2019-04-20 15:56:59 +0530

    Add files via upload

--- P2.py
+++ P2.py
@@ -1,48 +1,98 @@
 import socket
+import time
+import threading 
 
-def reciever():
-
-
-
-
-
+ports=[10001,10002,10003]
+MyPort=10002
+def getmsg():
+     global ports
+     global MyPort
 
      host=socket.gethostname()
-     port=10002
 
      s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-     s.bind((host,port))
-     print("socket binded to port",port)
+     s.bind((host,MyPort))
+     print("socket binded to port",MyPort)
 
      s.listen(5)
-     print("hmm, listening....")
+     print "hmm, listening...."
 
      c,addr=s.accept()
 
-     print("Connected to ",addr[0],':',addr[1])
+     print "Connected to ",addr[0],':',addr[1]
 
-     data = c.recv(1024)
+     msg = c.recv(1024)
 
-     print("Sender says: ",data.decode('ascii'))
+     print "Sender says: ",msg.decode('ascii')
 
-     if not data:
-         print('Bye')
+     if not msg:
+         print 'Bye'
+
      c.send("Thanks mate".encode('ascii'))
 
      c.close()
 
+     #time.sleep(0)
+     try:
+     #time.sleep(0)
+       #t1=thread.start_new_thread(shareMyHash, (hash(msg),)) 
+       #t2=thread.start_new_thread(listenToHashes, ()) 
+       t1 = threading.Thread(target=shareMyHash, args=(hash(msg),)) 
+       t2 = threading.Thread(target=listenToHashes, args=()) 
+     except Exception, e:
+        print e
+        print "something went wrong"
+     
+     t1.start()
+     t2.start()
+     t1.join()
+     t2.join()
+     return 0
+
+def shareMyHash(msg):
+    global ports
+    global MyPort
+
+
+    UDP_IP = "127.0.0.1"
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
+    for p in ports:
+         if p != MyPort:
+             print "sending my hash ",msg," to port ",p
+             sock.sendto(str(msg), (UDP_IP,p))
+
+def listenToHashes():
+    global ports
+    global MyPort
+
+ 
+    UDP_IP = "127.0.0.1"
+    UDP_PORT = MyPort
+
+    sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM) 
+    sock.bind((UDP_IP, UDP_PORT))
+
+    while True:
+        data, addr = sock.recvfrom(1024) 
+        print "received message:", data
 
 
 
 
 
+'''''    for port in ports:
+        if port != 10001:
+             host =socket.gethostname()
+
+             s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+             s.connect((host,port))
+
+             s.send(m.encode('ascii'))
 
 
-
-
-
-
-
+'''''
 
 
 
@@ -78,4 +128,4 @@ def reciever():
 	s.close()
 '''''
 if __name__ == '__main__':
-    reciever()
+    getmsg()
